f4bd3e3f3501a9c5d4ad99fbdabc6d326837d98c
Lucas Araujo <kirilucas@gmail.com>
2021-09-08 00:44:19 -0300

    Piece group creation

--- src/piece.service.ts
+++ src/piece.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { plainToClass } from 'class-transformer';
+import { GroupPiecesDTO } from './dtos/groupPieces.dto';
 import { NewPieceDTO } from './dtos/newPiece.dto';
 import { PieceModel } from './models/piece.model';
 
@@ -16,9 +17,9 @@ export class PieceService {
     model.save()
   }
 
-  groupPieces(newPieceDTO: NewPieceDTO) {
-    const model = plainToClass(PieceModel, newPieceDTO);
-    model.save()
+  groupPieces(groupPiecesDTO: GroupPiecesDTO) {
+    // const model = plainToClass(PieceModel, newPieceDTO);
+    // model.save()
     return 'Hello World!' as any;
   }
 }
